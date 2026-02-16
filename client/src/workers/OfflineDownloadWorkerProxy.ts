59df2a281d2b30daa41687871d4108f32b565c42
mori2163 <mori2430.8574@gmail.com>
2026-02-16 21:08:58 +0900

    Fix: [Client][Offline] コールバックオブジェクトのプロキシ化を簡素化

--- client/src/workers/OfflineDownloadWorkerProxy.ts
+++ client/src/workers/OfflineDownloadWorkerProxy.ts
@@ -39,13 +39,8 @@ class OfflineDownloadWorkerProxy implements IOfflineDownloadWorker {
      */
     public async download(options: IOfflineDownloadOptions, callbacks: IOfflineDownloadCallbacks): Promise<void> {
         const remote_worker = await this.remote_worker_promise;
-        // Comlink 越しに関数を渡すには proxy 化が必要
-        const proxied_callbacks: IOfflineDownloadCallbacks = {
-            onProgress: Comlink.proxy(callbacks.onProgress),
-            onCompleted: Comlink.proxy(callbacks.onCompleted),
-            onError: Comlink.proxy(callbacks.onError),
-        };
-        await remote_worker.download(options, proxied_callbacks);
+        // コールバックオブジェクト全体を proxy 化し、関数が構造化複製されないようにする
+        await remote_worker.download(options, Comlink.proxy(callbacks));
     }
 
 
